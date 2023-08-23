b3358e371a565e8d01ec01137c93d3f9d16dfed6
Carlos Diaz <carlos9559@gmail.com>
2023-08-22 22:43:19 -0500

    Calendar with validation and submit

---
+++ main react/src/components/DateText.jsx
@@ -0,0 +1,41 @@
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import PropTypes from 'prop-types';
+import { ErrorMessage } from 'formik';
+
+export function DateText ({fieldName, handleShowCalendar, handleChange, handleBlur, value, className}) {
+    return (
+        <>
+            <Form.Group className="d-flex flex-column mt-2">
+                <InputGroup className="field-form">
+                    <Form.Control
+                        placeholder={fieldName.toLowerCase()}
+                        aria-label={fieldName.toLowerCase()}
+                        type="text"
+                        className={`rounded-5 ${className}`}
+                        name={fieldName.toLowerCase().replace(/ /g, '')}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={value}
+                        onClick={handleShowCalendar}
+                    />
+                    <ErrorMessage
+                        name={fieldName.toLowerCase().replace(/ /g, '')}
+                        component="div"
+                        className="invalid-feedback"
+                    />
+                </InputGroup>
+            </Form.Group>
+        </>
+    );
+    
+}
+
+DateText.propTypes = {
+    fieldName: PropTypes.string.isRequired,
+    handleChange: PropTypes.func.isRequired,
+    handleBlur: PropTypes.func.isRequired,
+    handleShowCalendar: PropTypes.func.isRequired,
+    className: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+}
--- main react/src/components/ScheduleForm.jsx
+++ main react/src/components/ScheduleForm.jsx
@@ -7,7 +7,10 @@ import { useNavigate } from 'react-router-dom';
 import { ListSelect } from './ListSelect';
 import { FormDescription } from './FormDescription';
 import { NavSeparator } from './NavSeparator';
-import {Calendar, LocaleConfig} from 'react-native-calendars';
+import { useEffect, useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { DateText } from './DateText';
 
 const subjects = ["Math", "Science", "Spanish", "History", "English", "Art", "Music", "Physical Education", "Biology", "Chemistry", "Physics", "Geography", "Computer Science", "Economics", "Psychology", "Sociology", "Political Science", "Literature", "Philosophy", "Foreign Languages", "Health Education", "Environmental Science", "Civics", "Engineering", "Astronomy"];
 const locations = ["Teacher's location", "Student's location", "Videocall"]
@@ -22,16 +25,31 @@ const scheduleSchema = z
         errorMap: () => ({ message: 'Please select a valid location' })
     }),
     locationdescription: z.string(),
+    scheduledate: z.string()
 })
 export function ScheduleForm() {
     const navigate = useNavigate();
-    const [selectedDate, setSelectedDate] = useState('');
+    const [dateValue, onDateChange] = useState(new Date());
+    const [dateSelected, setDateSelected] = useState(`${dateValue.getFullYear()}-${dateValue.getMonth().toString().padStart(2, '0')}-${dateValue.getDate().toString().padStart(2, '0')}`);
+    const [showCalendar, setShowCalendar] = useState(false);
+
+    const handleShowCalendar = () => {
+        setShowCalendar(!showCalendar);
+        console.log(showCalendar,'++')
+    }
+    const handleCloseCalendar = () => {
+        setShowCalendar(false);
+    }
+    
+    // dateValue=`${dateValue.getFullYear()}-${dateValue.getMonth().toString().padStart(2, '0')}-${dateValue.getDate().toString().padStart(2, '0')}`
     const initialValues = {
         subject: '',
         topicdescription: '',
         location: '',
         locationdescription: '',
-      }
+        scheduledate: dateSelected,
+    }
+
     return (
         <>
             <Formik 
@@ -49,6 +67,21 @@ export function ScheduleForm() {
                             <div className='d-flex flex-column justify-content-start'>
                                 <ListSelect optionsList={subjects} fieldName='SUBJECT' handleChange={handleChange} handleBlur={handleBlur} value={values.subject} className={touched.subject && errors.subject ? ' is-invalid' : ''} />
                                 <FormDescription fieldName="topic description" handleChange={handleChange} handleBlur={handleBlur} val={values.topicdescription} classN={touched.topicdescription && errors.topicdescription ? 'is-invalid' : ''}/>
+                                <div>
+                                    <DateText fieldName='schedule date' handleShowCalendar={handleShowCalendar} handleChange={handleChange} handleBlur={handleBlur} value={values.scheduledate} className={touched.scheduledate && errors.scheduledate ? 'is-invalid' : ''}/>
+                                    <Calendar 
+                                        show={showCalendar}
+                                        onHide={handleCloseCalendar}
+                                        onChange={onDateChange} 
+                                        value={dateSelected}
+                                        onClickDay={
+                                        (dateValue) => {
+                                            setDateSelected(`${dateValue.getFullYear()}-${dateValue.getMonth().toString().padStart(2, '0')}-${dateValue.getDate().toString().padStart(2, '0')}`)
+                                            values.scheduledate=`${dateValue.getFullYear()}-${dateValue.getMonth().toString().padStart(2, '0')}-${dateValue.getDate().toString().padStart(2, '0')}`;
+                                        }}
+                                        minDate={new Date()}
+                                    />
+                                </div>
                             </div>
                             <div className='d-flex flex-column justify-content-start'>
                                 <NavSeparator />
@@ -56,14 +89,9 @@ export function ScheduleForm() {
                             <div className='d-flex flex-column justify-content-start'>
                                 <ListSelect optionsList={locations} fieldName='LOCATION' handleChange={handleChange} handleBlur={handleBlur} value={values.location} className={touched.location && errors.location ? ' is-invalid' : ''} />
                                 <FormDescription fieldName="location description" handleChange={handleChange} handleBlur={handleBlur} val={values.locationdescription} classN={touched.locationdescription && errors.locationdescription ? 'is-invalid' : ''}/>
-                                <Calendar
-                                    onDayPress={day => {
-                                        setSelectedDate(day.dateString);
-                                    }}
-                                    markedDates={{
-                                    [selectedDate]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
-                                    }}
-                                />
+                                <div>
+                                    
+                                </div>
                             </div>
                             <div className='d-flex flex-column justify-content-start'>
                                 <NavSeparator />

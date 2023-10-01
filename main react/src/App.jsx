07354956c3675cdb47638c649ea9ece229a479ed
darrubla <darrubla2296@gmail.com>
2023-09-30 22:37:57 -0500

    feat: login testeado y rutas protegidas

--- main react/src/App.jsx
+++ main react/src/App.jsx
@@ -1,42 +1,85 @@
 //import { useState } from 'react'
-import { Suspense } from 'react';
-import './App.css';
-import { UserProvider } from './containers/UserContext';
-import { Welcome } from './pages/Welcome';
-import { Route, Routes } from 'react-router-dom';
-import { Signin } from './pages/Signin';
-import { Signup } from './pages/Signup';
-import { StudentAccountInfo } from './pages/StudentAccountInfo';
-import { NotFound } from './pages/NotFound';
-import { Schedule } from './pages/Schedule';
-import { Overview } from './pages/Overview';
-import { TeacherView } from './pages/TeacherView';
-import { Container } from 'react-bootstrap';
-import { Index } from './pages/Index';
-import { Navigation } from './components/Navigation';
-import { Landing } from './pages/Landing';
+import { Suspense } from 'react'
+import './App.css'
+import { UserProvider } from './containers/UserContext'
+import { Welcome } from './pages/Welcome'
+import { Route, Routes } from 'react-router-dom'
+import { Signin } from './pages/Signin'
+import { Signup } from './pages/Signup'
+import { StudentAccountInfo } from './pages/StudentAccountInfo'
+import { NotFound } from './pages/NotFound'
+import { Schedule } from './pages/Schedule'
+import { Overview } from './pages/Overview'
+import { TeacherView } from './pages/TeacherView'
+import { Container } from 'react-bootstrap'
+import { Index } from './pages/Index'
+import { Navigation } from './components/Navigation'
+import { Landing } from './pages/Landing'
+import ProtectedRoute from './containers/ProtectedRoute'
 
 function App() {
   return (
     <UserProvider>
-      <Container fluid className="p-0">
+      <Container fluid className='p-0'>
         <Suspense fallback={null}>
           {false && <Index />}
           <Navigation />
           <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/home" element={<Landing />} />
-            <Route path="/signin/*" element={<Signin />} />
-            <Route path="/signup/*" element={<Signup />} />
-            <Route path="/schedule" element={<Schedule />} />
-            <Route path="/overview" element={<Overview />} />
-            <Route path="/teachers/:id" element={<TeacherView />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/my_account" element={<StudentAccountInfo />} />
+            <Route path='/signin/*' element={<Signin />} />
+            <Route path='/signup/*' element={<Signup />} />
+            <Route
+              path='/'
+              element={
+                <ProtectedRoute>
+                  <Landing />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/home'
+              element={
+                <ProtectedRoute>
+                  <Landing />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/schedule'
+              element={
+                <ProtectedRoute>
+                  <Schedule />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/overview'
+              element={
+                <ProtectedRoute>
+                  <Overview />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/teachers/:id'
+              element={
+                <ProtectedRoute>
+                  <TeacherView />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/my_account'
+              element={
+                <ProtectedRoute>
+                  <StudentAccountInfo />
+                </ProtectedRoute>
+              }
+            />
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </Suspense>
       </Container>
     </UserProvider>
-  );
+  )
 }
-export default App;
+export default App
